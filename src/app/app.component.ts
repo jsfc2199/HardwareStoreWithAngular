6c54aa4ad51e0270f151538b871fa6fd156b5c15
Juan Sebastian <jsfc2199@gmail.com>
2023-04-06 22:43:55 -0500

    Added Login Component with Bootstrap Styles and Fon Awesome Icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Hardware-Store';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('https://hardware-backend-production.up.railway.app/v1/api/all-providers').subscribe(data => {
+      console.log(data)
+    })
+  }
+
+  onClick() {
+    this.http.post('https://hardware-backend-production.up.railway.app/v1/api/saveProvider', {
+      "name": "prueba 2",
+      "number": "3692581470",
+      "passport": "ger78gherog7"
+    }).subscribe(data=>{
+      console.log(data); //es obligatorio suscribirse en el metodo post para que todos los interesados escuchen por fuera, como un emit
+
+    })
+    console.log('entre a post');
+
+  }
 }
